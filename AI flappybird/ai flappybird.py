807f89887902db96aab41b26edc997914f391886
KaizanS <104655398+KaizanS@users.noreply.github.com>
2023-09-06 16:16:33 -0700

    Add files via upload

--- AI flappybird/ai flappybird.py
+++ AI flappybird/ai flappybird.py
@@ -8,6 +8,7 @@ pygame.font.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
+GEN = 0
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
@@ -175,7 +176,7 @@ class Base:
 		win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
 	win.blit(BG_IMG, (0,0))
 
 	for pipe in pipes:
@@ -183,6 +184,9 @@ def draw_window(win, birds, pipes, base, score):
 
 	text = STAT_FONT.render("Score = " + str(score), 1, (255,255,255))
 	win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+	text = STAT_FONT.render("Gen = " + str(gen), 1, (255,255,255))
+	win.blit(text, (10, 10))
 
 	base.draw(win)
 
@@ -200,6 +204,8 @@ def main(genomes, config):
 	birds = []
 	nets = []
 	ge = []
+	global GEN 
+	GEN += 1
 
 	for _, g in genomes:
 		net = neat.nn.FeedForwardNetwork.create(g, config)
@@ -275,7 +281,7 @@ def main(genomes, config):
 				ge.pop(x)
 
 		base.move()		
-		draw_window(win, birds, pipes, base, score)
+		draw_window(win, birds, pipes, base, score, GEN)
 	
 
 def run(config_path):
